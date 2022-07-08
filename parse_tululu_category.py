a7a0500debdcb7426f91debc882e923f9887118a
Evgen-Mamaev <97582067+Evgen-Mamaev@users.noreply.github.com>
2022-07-08 11:43:08 +0700

    get url 250 books fantasy genre from 10 pages

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -4,10 +4,12 @@ import requests
 from bs4 import BeautifulSoup
 
 url_tululu = 'https://tululu.org/'
-url_fantasy_genre_books = 'https://tululu.org/l55/1'
-response_url_fantasy_genre_books = requests.get(url_fantasy_genre_books)
-response_url_fantasy_genre_books.raise_for_status()
-soup = BeautifulSoup(response_url_fantasy_genre_books.text, 'lxml')
-book_numbers = [book.find('a')['href'] for book in soup.find_all('table', class_='d_book')]
-url_books = [urljoin(url_tululu, book_number) for book_number in book_numbers]
-print(*url_books, sep='\n')
+
+for number_page in range(1, 11):
+    url_fantasy_genre_books = f'https://tululu.org/l55/{number_page}'
+    response_url_fantasy_genre_books = requests.get(url_fantasy_genre_books)
+    response_url_fantasy_genre_books.raise_for_status()
+    soup = BeautifulSoup(response_url_fantasy_genre_books.text, 'lxml')
+    book_numbers = [book.find('a')['href'] for book in soup.find_all('table', class_='d_book')]
+    url_books = [urljoin(url_tululu, book_number) for book_number in book_numbers]
+    print(*url_books, sep='\n')
