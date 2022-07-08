771a81db7a086f1c62c28d3e8cdb477fb5933246
Evgen-Mamaev <97582067+Evgen-Mamaev@users.noreply.github.com>
2022-07-08 10:55:28 +0700

    get url 25 books fantasy genre

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -8,6 +8,6 @@ url_fantasy_genre_books = 'https://tululu.org/l55/1'
 response_url_fantasy_genre_books = requests.get(url_fantasy_genre_books)
 response_url_fantasy_genre_books.raise_for_status()
 soup = BeautifulSoup(response_url_fantasy_genre_books.text, 'lxml')
-book_number = soup.find('table', class_='d_book').find('a')['href']
-url_book = urljoin(url_tululu, book_number)
-print(url_book)
+book_numbers = [book.find('a')['href'] for book in soup.find_all('table', class_='d_book')]
+url_books = [urljoin(url_tululu, book_number) for book_number in book_numbers]
+print(*url_books, sep='\n')
