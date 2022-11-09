11ad661712079b24a686e6e27e7bb59fd39ed84b
Evgen <Evgenan86@gmail.com>
2022-11-09 14:52:57 +0700

    added fixes

--- parse_tululu_category.py
+++ parse_tululu_category.py
@@ -12,12 +12,19 @@ from pathvalidate import sanitize_filename
 logger = logging.getLogger(__file__)
 
 
-def saves_book_txt(response, filename, folder):
+def check_for_redirect(response_history):
+    if response_history:
+        raise requests.exceptions.HTTPError
+
+
+def saves_book_txt(url, filename, folder, payload):
+    response_url_download_txt = requests.get(url, params=payload)
+    response_url_download_txt.raise_for_status()
     os.makedirs(folder, exist_ok=True)
     filename = sanitize_filename(filename)
     filepath = os.path.join(folder, filename)
     with open(filepath, 'wb') as file:
-        file.write(response.content)
+        file.write(response_url_download_txt.content)
 
 
 def download_book_cover(img_url, folder):
@@ -49,20 +56,13 @@ def parse_book_page(response):
     return book_page
 
 
-def get_response_url_download_txt(url, payload):
-    response_url_download_txt = requests.get(url, params=payload)
-    response_url_download_txt.raise_for_status()
-    return response_url_download_txt
-
-
-def get_book_urls_answers_from_page(page_number):
+def get_book_urls(page_number):
     tululu_url = 'https://tululu.org/'
     fantasy_genre_book_urls = urljoin(tululu_url, f'/l55/{page_number}')
     fantasy_genre_book_response_urls = requests.get(fantasy_genre_book_urls)
     fantasy_genre_book_response_urls.raise_for_status()
     soup = BeautifulSoup(fantasy_genre_book_response_urls.text, 'lxml')
-    book_numbers = [book_number['href'] for book_number in soup.select('.bookimage a')]
-    book_urls = [urljoin(tululu_url, book_number) for book_number in book_numbers]
+    book_urls = [urljoin(tululu_url, book_number['href']) for book_number in soup.select('.bookimage a')]
     return book_urls
 
 
@@ -83,14 +83,8 @@ def write_json(json_file, folder, filename):
     os.makedirs(folder, exist_ok=True)
     filename = sanitize_filename(filename)
     filepath = os.path.join(folder, filename)
-    try:
-        with open(filepath, 'r', encoding='utf8') as my_file:
-            file = json.load(my_file)
-    except FileNotFoundError:
-        file = []
-    file.append(json_file)
-    with open(filepath, 'w', encoding='utf8') as my_file:
-        json.dump(file, my_file, ensure_ascii=False, indent=2)
+    with open(filepath, 'a', encoding='utf8') as my_file:
+        json.dump(json_file, my_file, ensure_ascii=False, indent=2)
 
 
 if __name__ == '__main__':
@@ -102,27 +96,28 @@ if __name__ == '__main__':
     download_start_page = args.start_page
     download_stop_page = args.end_page
 
-    json_file = []
+    book_pages = []
     for page_number in range(download_start_page, download_stop_page):
         try:
-            book_urls = get_book_urls_answers_from_page(page_number)
+            book_urls = get_book_urls(page_number)
             for book_url in book_urls:
                 try:
                     book_number = book_url.split('/')[-2].replace('b', '')
 
-                    response_book_url = requests.get(book_url)
-                    response_book_url.raise_for_status()
+                    book_url_response = requests.get(book_url)
+                    book_url_response.raise_for_status()
 
-                    book_page = parse_book_page(response_book_url)
+                    check_for_redirect(book_url_response.history)
+
+                    book_page = parse_book_page(book_url_response)
 
                     book_page['book_path'] = ' '
                     if not args.skip_txt:
-                        url_download_txt = 'https://tululu.org/txt.php'
+                        txt_download_url = 'https://tululu.org/txt.php'
                         payload = {'id': book_number}
-                        response_url_download_txt = get_response_url_download_txt(url_download_txt, payload)
                         filename = f"{book_number}. {book_page.get('title')}.txt"
                         folder = os.path.join(args.dest_folder, 'book')
-                        saves_book_txt(response_url_download_txt, filename, folder)
+                        saves_book_txt(txt_download_url, filename, folder, payload)
                         book_page['book_path'] = os.path.join(folder, filename)
 
                     img_filename = book_page.get('img_path')
@@ -133,7 +128,7 @@ if __name__ == '__main__':
                         download_book_cover(img_url, folder)
                         book_page['img_path'] = os.path.join(folder, img_filename)
 
-                    json_file.append(book_page)
+                    book_pages.append(book_page)
 
                     logger.info(f'Book number {book_number} loaded')
                 except requests.exceptions.ConnectionError:
@@ -146,4 +141,4 @@ if __name__ == '__main__':
 
     json_folder = args.dest_folder
     json_filename = f'{args.json_path}.json'
-    write_json(json_file, json_folder, json_filename)
+    write_json(book_pages, json_folder, json_filename)
